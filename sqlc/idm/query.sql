a8dd1d400d3b88e6d69c994341e44c39e8bd810d
Lei Wang <tendant@gmail.com>
2024-01-23 11:21:09 -0800

    update query

--- sqlc/idm/query.sql
+++ sqlc/idm/query.sql
@@ -3,4 +3,7 @@ SELECT uuid, username, password
 FROM idm_users
 where username = $1;
 
--- name: Createuser
+-- name: FindTraining
+SELECT uuid, name, created_at
+FROM training
+where name like '%demo%'
